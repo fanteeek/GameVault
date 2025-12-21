8d5bb61b52934f218aaa0bb338c3df0d7f447fbd
fanteeek <fanteeek2000@gmail.com>
2025-12-21 05:37:17 +0300

    fix: add method 'version_to_tuple'

--- core/updater.py
+++ core/updater.py
@@ -1,5 +1,4 @@
 import requests
-import os
 import sys
 import subprocess
 
@@ -9,18 +8,33 @@ class UpdaterService:
     GITHUB_API_URL = "https://api.github.com/repos/fanteeek/GameVault/releases/latest"
 
     @staticmethod
+    def version_to_tuple(v):
+        try:
+            v = v.lower().lstrip('v')
+            import re
+            clean_v = re.split(r'[^0-9.]', v)[0]
+            parts = clean_v.strip('.').split('.')
+            return tuple(map(int, parts))
+        except Exception:
+            return (0, 0, 0)
+
+    
+    @staticmethod
     def check_for_updates(current_version):
         try:
             response = requests.get(UpdaterService.GITHUB_API_URL, timeout=5)
             if response.status_code == 200:
                 data = response.json()
-                latest_version = data['tag_name'].replace('v', '')
+                latest_version = data['tag_name']
                 
-                if latest_version > current_version:
+                v_latest = UpdaterService.version_to_tuple(latest_version)
+                v_current = UpdaterService.version_to_tuple(current_version)
+                
+                if v_latest > v_current:
                     return {
                         "update_available": True,
                         "latest_version": latest_version,
-                        "download_url": data['assets'][0]['browser_download_url'], # Ссылка на .exe
+                        "download_url": data['assets'][0]['browser_download_url'],
                         "changelog": data['body']
                     }
         except Exception as e:
@@ -33,7 +47,6 @@ class UpdaterService:
             app_dir = FileUtils.get_app_dir()
             setup_path = app_dir / "GameVault_Update_Setup.exe"
             
-            # 1. Скачивание
             response = requests.get(download_url, stream=True)
             with open(setup_path, 'wb') as f:
                 for chunk in response.iter_content(chunk_size=8192):
